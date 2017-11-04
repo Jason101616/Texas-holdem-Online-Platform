0ec042c39ea1dd094fafe811a1fdc982b46b57e1
liushuangning <liusn813@gmail.com>
2017-11-04 13:39:45 -0400

    built the login page and sign page

--- src/webprj/texas/views.py
+++ src/webprj/texas/views.py
@@ -7,4 +7,12 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     context = {}
-    return render(request, 'websocket_test_page.html', context)
+    return render(request, 'homepage.html', context)
+
+def signup(request):
+    context = {}
+    return render(request, 'signup.html', context)
+
+def dashboard(request):
+    context = {}
+    return render(request, 'dashboard.html', context)
