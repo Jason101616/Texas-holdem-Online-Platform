2371ccc8ce11c5e9e3350b81934143b477c40d42
liushuangning <liusn813@gmail.com>
2017-11-05 15:23:54 -0500

    set the card id

--- src/webprj/texas/test_consumer.py
+++ src/webprj/texas/test_consumer.py
@@ -3,29 +3,6 @@ import json
 from channels.sessions import channel_session
 from urllib.parse import parse_qs
 
-#demo 1
-# def ws_echo(message):
-#     print(message.content)
-#     message.reply_channel.send({
-#         'text': message.content['text'] + ' done!',
-#     })
-#
-# @channel_session
-# def ws_connect(message):
-#     # Accept connection
-#     message.reply_channel.send({"accept": True})
-#     # Parse the query string
-#     params = parse_qs(message.content["query_string"])
-#     if b"username" in params:
-#         # Set the username in the session
-#         message.channel_session["username"] = params[b"username"][0].decode("utf8")
-#         # Add the user to the room_name group
-#         Group("test").add(message.reply_channel)
-#     else:
-#         # Close the connection.
-#         message.reply_channel.send({"close": True})
-
-
 #demo 2
 @channel_session
 def ws_msg(message):
@@ -52,22 +29,3 @@ def ws_add(message):
 @channel_session
 def ws_disconnect(message):
     Group("test").discard(message.reply_channel)
-
-# Connected to websocket.connect
-# def ws_add(message):
-#     # Accept the connection
-#     print("in ws_add", message)
-#     message.reply_channel.send({"accept": True})
-#     # Add to the chat group
-#     Group("chat").add(message.reply_channel)
-#
-# # Connected to websocket.receive
-# def ws_msg(message):
-#     print("in ws_msg", message)
-#     Group("chat").send({
-#         "text": "[user] %s" % message.content['text'],
-#     })
-#
-# # Connected to websocket.disconnect
-# def ws_disconnect(message):
-#     Group("chat").discard(message.reply_channel)
