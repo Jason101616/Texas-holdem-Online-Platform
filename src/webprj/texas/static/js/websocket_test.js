6f5d4968176e0228c879ee1654e8668c6f81788c
Du Zhang <duz1@andrew.cmu.edu>
2017-11-05 14:16:38 -0500

    seemly no bug version

--- src/webprj/texas/static/js/websocket_test.js
+++ src/webprj/texas/static/js/websocket_test.js
@@ -37,26 +37,38 @@ $(document).ready(function() {
     }
   });
 
-  console.log('hello world!');
-  // Create a new WebSocket
-    ws = new WebSocket("ws://" + window.location.host + "/test/");
-    // Make it show an alert when a message is received
-    ws.onmessage = function(message) {
-        console.log(message);
-        console.log(message.data + ' hi!');
+  // console.log('hello world!');
+  // // Create a new WebSocket
+  //   ws = new WebSocket("ws://" + window.location.host + "/test/");
+  //   // Make it show an alert when a message is received
+  //   ws.onmessage = function(message) {
+  //       console.log(message);
+  //       console.log(message.data + ' hi!');
+  //   };
+  //   // Send a new message when the WebSocket opens
+  //   ws.onopen = function() {
+  //       ws.send('Hello, world');
+  //   };
+  //   if (ws.readyState === WebSocket.OPEN)
+  //     ws.onopen();
+  //   console.log('ws finished!');
+
+    // Note that the path doesn't matter right now; any WebSocket
+// connection gets bumped over to WebSocket consumers
+    socket = new WebSocket("ws://" + window.location.host + "/chat/");
+    socket.onmessage = function(e) {
+        alert(e.data);
     };
-    // Send a new message when the WebSocket opens
-    ws.onopen = function() {
-        ws.send('Hello, world');
+    socket.onopen = function() {
+        socket.send("hello world");
     };
-    if (ws.readyState === WebSocket.OPEN) ws.onopen();
-    console.log('ws finished!');
+    // Call onopen directly if socket is already open
+    if (socket.readyState === WebSocket.OPEN) socket.onopen();
 
     $('#get_card').on('click', function(event) {
         event.preventDefault(); // Prevent form from being submitted
         console.log("click!");
         ws.send("click");
-
   });
 
 });
--- src/webprj/texas/test_consumer.py
+++ src/webprj/texas/test_consumer.py
@@ -4,46 +4,70 @@ from channels.sessions import channel_session
 from urllib.parse import parse_qs
 
 #demo 1
-def ws_echo(message):
-    print(message.content)
-    message.reply_channel.send({
-        'text': message.content['text'] + ' done!',
-    })
-
-@channel_session
-def ws_connect(message):
-    # Accept connection
-    message.reply_channel.send({"accept": True})
-    # Parse the query string
-    params = parse_qs(message.content["query_string"])
-    if b"username" in params:
-        # Set the username in the session
-        message.channel_session["username"] = params[b"username"][0].decode("utf8")
-        # Add the user to the room_name group
-        Group("test").add(message.reply_channel)
-    else:
-        # Close the connection.
-        message.reply_channel.send({"close": True})
+# def ws_echo(message):
+#     print(message.content)
+#     message.reply_channel.send({
+#         'text': message.content['text'] + ' done!',
+#     })
+#
+# @channel_session
+# def ws_connect(message):
+#     # Accept connection
+#     message.reply_channel.send({"accept": True})
+#     # Parse the query string
+#     params = parse_qs(message.content["query_string"])
+#     if b"username" in params:
+#         # Set the username in the session
+#         message.channel_session["username"] = params[b"username"][0].decode("utf8")
+#         # Add the user to the room_name group
+#         Group("test").add(message.reply_channel)
+#     else:
+#         # Close the connection.
+#         message.reply_channel.send({"close": True})
 
 
 #demo 2
 @channel_session
 def ws_msg(message):
+    print("in ws_msg", message)
     Group("test").send({
         "text": json.dumps({
             "text": message["text"],
-            "username": message.channel_session["username"],
+            "username": message.channel_session['username'],
         }),
     })
 
 # Connected to websocket.connect
+@channel_session
 def ws_add(message):
+    print("in ws_add", message)
     # Accept the incoming connection
     message.reply_channel.send({"accept": True})
+    message.channel_session['username'] = "test"
     # Add them to the chat group
     Group("test").add(message.reply_channel)
+
 
 # Connected to websocket.disconnect
 @channel_session
 def ws_disconnect(message):
     Group("test").discard(message.reply_channel)
+
+# Connected to websocket.connect
+# def ws_add(message):
+#     # Accept the connection
+#     print("in ws_add", message)
+#     message.reply_channel.send({"accept": True})
+#     # Add to the chat group
+#     Group("chat").add(message.reply_channel)
+#
+# # Connected to websocket.receive
+# def ws_msg(message):
+#     print("in ws_msg", message)
+#     Group("chat").send({
+#         "text": "[user] %s" % message.content['text'],
+#     })
+#
+# # Connected to websocket.disconnect
+# def ws_disconnect(message):
+#     Group("chat").discard(message.reply_channel)
