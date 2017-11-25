0ee60f05ec4eace689e0b9561dbb1ca5ade223c5
liushuangning <liusn813@gmail.com>
2017-11-25 00:27:30 -0500

    tried popover

--- src/webprj/texas/static/js/lobby.js
+++ src/webprj/texas/static/js/lobby.js
@@ -30,21 +30,31 @@ $(document).ready(function () {
         }
     });
 
-    $('#newplay').on('click', function(event) {
-            event.preventDefault(); // Prevent form from being submitted
-            var room_id = prompt("Please enter a desk name:", ""); 
-            if (room_id){
-                $.ajax({
-                    type: 'post',
-                    url: 'newplay/' + room_id,
-                    data: '',
-                    success: function (data) {
-                        alert("Successfully created room: " + room_id);
-
-                        button = "<span><a class = 'smallbtn' id = 'desk-" + room_id + "' href = 'playroom/" + room_id + "'>" + room_id + "</a></span>";
-                        $("#newplay").before(button);
-                    }
-                })
+    $(function () {
+        $('#newplay').popover({
+            html: true,
+            title: function () {
+                //return $(this).parent().find('.head').html();
+                return "create a new desk";
+            },
+            content: function () {
+                debugger;
+                return $('#newplay_form').html();
             }
         });
+    });
+
+    /*$('#newplay').on('click', function(event) {
+        $.ajax({
+            type: 'post',
+            url: 'newplay/' + room_id,
+            data: '',
+            success: function (data) {
+                alert("Successfully created room: " + room_id);
+
+                button = "<span><a class = 'smallbtn' id = 'desk-" + room_id + "' href = 'playroom/" + room_id + "'>" + room_id + "</a></span>";
+                $("#newplay").before(button);
+            }
+        })
+    });*/
 });
